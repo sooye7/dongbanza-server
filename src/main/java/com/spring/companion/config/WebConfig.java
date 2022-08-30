cefea7eb742a6f0808e14d2ae66d9ca1242b977e
sooye7 <sooye7@naver.com>
2022-08-30 15:31:31 +0900

    FIX: CORS 해결

---
+++ src/main/java/com/spring/companion/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.spring.companion.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST");  // 모든 Origin 허락
+
+
+    }
+}
